bd94cdaccd8926784c17c2da8aa7ac188721b7a1
lxcher <aleksejcerenkevic@gmail.com>
2022-07-14 16:29:14 +0300

    fix: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
+const OPEN_BRACKETS = ['(', '[', '{', '|', '1', '3', '5', '7', '8']
+const BRACKETS_PAIRS = {
+    [')']: '(',
+    [']']: '[',
+    ['}']: '{',
+    ['|']: '|',
+    ['2']: '1',
+    ['4']: '3',
+    ['6']: '5',
+    ['7']: '7',
+    ['8']: '8',
+}
+
+// const SECOND_SYMBOL_CHECK = ['|', '7', '8']
+
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = []
+  for (let i = 0; i < str.length; i++) {
+      let currentSymbol = str[i]
+      if (OPEN_BRACKETS.includes(currentSymbol) && !'|78'.includes(stack[stack.length - 1])) {
+          stack.push(currentSymbol)
+      } else {
+          if (stack.length === 0) {
+              return false
+          } else {
+              let topSymbol = stack[stack.length - 1]
+              if (topSymbol === BRACKETS_PAIRS[currentSymbol]) {
+                  stack.pop()
+              }
+          }
+      }
+  }
+  if (stack.length === 0) {
+      return true
+  } else {
+      return false
+  }
 }
